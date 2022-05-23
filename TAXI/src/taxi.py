537926b265e7459c23bbb12d969a59698390d210
Seif elbouhy <seif.elbouhy@gmail.com>
2022-05-23 16:05:33 +0200

    remove omxplayer
    using vlc

--- TAXI/src/taxi.py
+++ TAXI/src/taxi.py
@@ -4,11 +4,10 @@ import time
 import datetime
 import os
 import subprocess
+import vlc
 import RPi.GPIO as GPIO
 
 from subprocess import Popen
-
-
 
 argparser = argparse.ArgumentParser(
     description='Taxi')
@@ -23,9 +22,8 @@ args = argparser.parse_args()
 city = args.city
 
 
-Path_To_TaxiLogo = "sudo fbi -T 1 -noverbose -a /home/pi/TAXI/img/TaxiLogo.png &"
-Path_To_TaxiVideo = "/home/pi/TAXI/vid/" + city + "/TE_SR_Taxi.mp4"
-Path_To_ArrowLogo = "sudo fbi -T 1 -noverbose -a /home/pi/TAXI/img/ArrowLogo.png &"
+Path_To_TaxiLogo = "cvlc /home/pi/TAXI/img/TaxiLogo.png  -f --no-osd --loop &"
+Path_To_ArrowLogo = "cvlc /home/pi/TAXI/img/ArrowLogo.png  -f --no-osd --loop &"
 
 SENSOR_PIN = 23
 RELAY_PIN = 22
@@ -38,23 +36,39 @@ GPIO.setup(SENSOR_PIN, GPIO.IN)
 GPIO.setup(RELAY_PIN, GPIO.OUT)
 GPIO.output(RELAY_PIN, GPIO.HIGH)
 #media = vlc.MediaPlayer(Path_To_TaxiVideo)
-os.system(Path_To_TaxiLogo)
-time.sleep(15)
-i= 0
+
+
+
+vid_path = 'TE_SR_Taxi.mp4' 
+pic_path = 'TaxiLogo.png'
+pic2_path = 'ArrowLogo.png'
+
+cmnd = 'cvlc {0} -f --no-osd &'
+vid_command = cmnd.format(f"~/TAXI/vid/hh/{vid_path}")
+pic_command = cmnd.format(f"~/TAXI/img/{pic_path}")
+pic2_command = cmnd.format(f"~/TAXI/img/{pic2_path}")
+
+
+os.system(pic_command)
+time.sleep(5)
 
 #Motion Detection boolean check 
 def checkMotion(motion):
     if motion == 1:
         return True
+        
     else:
         return False
 
 #play video if sensor is detected
 def playVideo():
-    os.system("sudo killall -9 fbi")
-    Popen(['omxplayer', '-b', Path_To_TaxiVideo])
     
+    os.system("sudo pkill vlc")
+    os.system(vid_command)
+  
+    #Popen(['omxplayer', '-b', Path_To_TaxiVideo])
     time.sleep(25)
+    os.system("sudo pkill vlc")
 
     # Logging: door was opened
     GPIO.output(RELAY_PIN, GPIO.LOW)
@@ -65,8 +79,7 @@ def main():
 
     while True:
 
-        i=GPIO.input(SENSOR_PIN)
-        detected = checkMotion(i)
+        detected = checkMotion(GPIO.input(SENSOR_PIN))
         if not detected:
             time.sleep(2)
             print ("Motion is not Detected")
@@ -74,7 +87,7 @@ def main():
         else:
             print("Motion is Detected")
             playVideo()
-            os.system(Path_To_ArrowLogo)
+            os.system(pic2_command)
             break
 
 
